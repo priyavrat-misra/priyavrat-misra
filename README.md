cda302bc72c6c4fee349388052407ecd1acc01c8
Priyavrat Misra <PriyavratMisra123@gmail.com>
2022-06-19 17:32:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Hey there, I'm Priyavrat 👋
+To know more about me, visit my [portfolio website](https://p-v.pages.dev).
---
+++ generate.py
@@ -0,0 +1,22 @@
+import jinja2
+import yaml
+import feedparser
+from datetime import datetime
+
+# Set up templates
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+template = templateEnv.get_template("TEMPLATE.md")
+
+# Load config
+with open("config.yaml") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+# Get latest blog post
+blog_rss = feedparser.parse("https://p-v.pages.dev/blog/rss.xml")
+config["latest_blog_posts"] = blog_rss["entries"][0]
+for post in config["latest_blog_posts"]:
+    post["published"] = datetime.strptime(post["published"], "%a, %d %b %Y %H:%M:%S %z")
+
+# Render remplate
+print(template.render(**config))
